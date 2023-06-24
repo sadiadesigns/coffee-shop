fd5e675d5b49774671048cdb8be1fa48dded58ce
Sadia Ahmed <132383618+sadiadesigns@users.noreply.github.com>
2023-06-24 23:35:28 +0600

    Completion of the project with proper comments

--- features.h
+++ features.h
@@ -7,6 +7,7 @@
 #define MAX_COFFEE_NAME_LENGTH 50
 #define MAX_FILENAME_LENGTH 100
 
+// Function to add a new coffee to the inventory
 void addCoffee() {
     char coffeeName[MAX_COFFEE_NAME_LENGTH];
     float price;
@@ -21,9 +22,10 @@ void addCoffee() {
     printf("Enter the quantity of the coffee: ");
     scanf("%d", &quantity);
 
+    // Open the database file for reading
     FILE* file = fopen("database.txt", "r");
     if (file == NULL) {
-        createDatabaseFile();
+        createDatabaseFile(); // Create the database file if it doesn't exist
         file = fopen("database.txt", "r");
         if (file == NULL) {
             printf("Error opening the file.\n");
@@ -31,6 +33,7 @@ void addCoffee() {
         }
     }
 
+    // Determine the ID for the new coffee entry
     int id = 0;
     char line[100];
     while (fgets(line, sizeof(line), file)) {
@@ -39,18 +42,21 @@ void addCoffee() {
 
     fclose(file);
 
+    // Open the database file in append mode to add the new coffee entry
     file = fopen("database.txt", "a");
     if (file == NULL) {
         printf("Error opening the file.\n");
         return;
     }
 
+    // Write the new coffee entry to the database file
     fprintf(file, "%d,%s,%.2f,%d\n", id + 1, coffeeName, price, quantity);
     fclose(file);
 
     printf("Coffee added successfully!\n");
 }
 
+// Function to sell a coffee from the inventory
 void sellCoffee() {
     printf("**************    SELL COFFEE   *****************\n");
     printf("**************************************************\n");
@@ -63,6 +69,7 @@ void sellCoffee() {
     printf("Enter the quantity to sell: ");
     scanf("%d", &quantity);
 
+    // Open the database file for reading and writing
     FILE* file = fopen("database.txt", "r+");
     if (file == NULL) {
         printf("Error opening the file.\n");
@@ -74,6 +81,7 @@ void sellCoffee() {
     int updatedQuantity = 0;
     long int currentPosition = 0;
 
+    // Iterate through each line in the file to find the coffee with the given ID
     while (fgets(line, sizeof(line), file)) {
         int coffeeId;
         char coffeeName[MAX_COFFEE_NAME_LENGTH];
@@ -91,7 +99,7 @@ void sellCoffee() {
                 fprintf(file, "%d,%s,%.2f,%d\n", coffeeId, coffeeName, price, updatedQuantity);
                 printf("Coffee sold successfully!\n");
 
-                // Update balance
+                // Update the balance file with the total sales
                 FILE* balanceFile = fopen("balance.txt", "a");
                 if (balanceFile == NULL) {
                     printf("Error opening the balance file.\n");
@@ -118,12 +126,14 @@ void sellCoffee() {
     fclose(file);
 }
 
+// Function to display the inventory
 void showInventory() {
     printf("**************    INVENTORY    ******************\n");
     printf("**************************************************\n");
     printf("ID\tCoffee Name\tPrice\tQuantity\n");
     printf("===============================================\n");
 
+    // Open the database file for reading
     FILE* file = fopen("database.txt", "r");
     if (file == NULL) {
         printf("Error opening the file.\n");
@@ -144,10 +154,12 @@ void showInventory() {
     fclose(file);
 }
 
+// Function to display the total sales balance
 void showBalance() {
     printf("****************    BALANCE    ******************\n");
     printf("**************************************************\n");
 
+    // Open the balance file for reading
     FILE* balanceFile = fopen("balance.txt", "r");
     if (balanceFile == NULL) {
         printf("Error opening the balance file.\n");
@@ -166,6 +178,7 @@ void showBalance() {
     printf("Total Sales: %.2f\n", totalSales);
 }
 
+// Function to remove a coffee from the inventory
 void removeCoffee() {
     printf("*************    REMOVE COFFEE    ****************\n");
     printf("**************************************************\n");
@@ -174,12 +187,14 @@ void removeCoffee() {
     printf("Enter the ID of the coffee to remove: ");
     scanf("%d", &id);
 
+    // Open the database file for reading and writing
     FILE* file = fopen("database.txt", "r+");
     if (file == NULL) {
         printf("Error opening the file.\n");
         return;
     }
 
+    // Create a temporary file for rewriting the database without the removed coffee
     FILE* tempFile = fopen("temp.txt", "w");
     if (tempFile == NULL) {
         printf("Error creating temporary file.\n");
@@ -190,6 +205,7 @@ void removeCoffee() {
     char line[100];
     int found = 0;
 
+    // Iterate through each line in the file and copy lines to the temporary file except for the one with the given ID
     while (fgets(line, sizeof(line), file)) {
         int coffeeId;
         char coffeeName[MAX_COFFEE_NAME_LENGTH];
@@ -208,6 +224,7 @@ void removeCoffee() {
     fclose(file);
     fclose(tempFile);
 
+    // Replace the original database file with the temporary file if the coffee was found and removed
     if (found) {
         remove("database.txt");
         rename("temp.txt", "database.txt");
--- login.h
+++ login.h
@@ -6,31 +6,45 @@
 #define MAX_USERNAME_LENGTH 20
 #define MAX_PASSWORD_LENGTH 20
 
+// Function to authenticate a user based on provided username and password
 int authenticateUser(char* username, char* password);
+
+// Function to display the login panel and handle user authentication
 void showLoginPanel();
 
+// Implementation of the showLoginPanel function
 void showLoginPanel() {
     char username[MAX_USERNAME_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
+    int attempts = 0;
+    int maxAttempts = 3;
 
     printf("************      LOGIN PANEL       **************\n");
     printf("**************************************************\n");
-    printf("Enter your username: ");
-    scanf("%s", username);
-    printf("Enter your password: ");
-    scanf("%s", password);
 
-    if (authenticateUser(username, password)) {
-        printf("Login successful!\n\n");
-    } else {
-        printf("Invalid username or password. Exiting program.\n");
-        exit(0);
+    while (attempts < maxAttempts) {
+        printf("Enter your username: ");
+        scanf("%s", username);
+        printf("Enter your password: ");
+        scanf("%s", password);
+
+        if (authenticateUser(username, password)) {
+            printf("Login successful!\n\n");
+            return; // Exit the function when login is successful
+        } else {
+            printf("Invalid username or password. Please try again.\n");
+            attempts++;
+        }
     }
+
+    printf("Maximum number of login attempts reached. Exiting program.\n");
+    exit(0);
 }
 
+// Implementation of the authenticateUser function
 int authenticateUser(char* username, char* password) {
     if (strcmp(username, "admin") == 0 && strcmp(password, "admin") == 0) {
-        return 1;
+        return 1; // Return 1 if the provided username and password match the admin credentials
     }
-    return 0;
+    return 0; // Return 0 if the authentication fails
 }
